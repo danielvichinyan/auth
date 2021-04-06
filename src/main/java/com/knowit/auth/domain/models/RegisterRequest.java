a87cb9b0fa3e8d620f66f8ad31ca1218781b8562
danielvichinyan <vichinyan.daniel@gmail.com>
2021-04-06 13:50:33 +0300

    Added kafka to the project as well as spring cloud. Implemented service logic for login and register as well as kafka logic.

--- src/main/java/com/knowit/auth/domain/models/RegisterRequest.java
+++ src/main/java/com/knowit/auth/domain/models/RegisterRequest.java
@@ -11,4 +11,24 @@ public class RegisterRequest {
     private String firstName;
 
     private String lastName;
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public String getConfirmPassword() { return confirmPassword; }
+
+    public void setConfirmPassword(String confirmPassword) { this.confirmPassword = confirmPassword; }
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
 }
--- src/main/java/com/knowit/auth/services/UserServiceImpl.java
+++ src/main/java/com/knowit/auth/services/UserServiceImpl.java
@@ -1,16 +1,23 @@
 package com.knowit.auth.services;
 
+import com.knowit.auth.constants.KafkaConstants;
 import com.knowit.auth.domain.entities.User;
 import com.knowit.auth.domain.models.LoginRequest;
 import com.knowit.auth.domain.models.RegisterRequest;
 import com.knowit.auth.domain.models.RegisterResponse;
+import com.knowit.auth.domain.models.RegisterUserModel;
 import com.knowit.auth.exceptions.PasswordsDoNotMatchException;
 import com.knowit.auth.exceptions.WrongCredentialsException;
 import com.knowit.auth.jwt.JwtImpl;
 import com.knowit.auth.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Supplier;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -23,20 +30,38 @@ public class UserServiceImpl implements UserService {
 
     private final JwtImpl jwtUtils;
 
+    private final StreamBridge streamBridge;
+
     public UserServiceImpl(
             UserRepository userRepository,
             ModelMapper modelMapper,
             PasswordEncoder passwordEncoder,
-            JwtImpl jwtUtils) {
+            JwtImpl jwtUtils, StreamBridge streamBridge) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtils = jwtUtils;
+        this.streamBridge = streamBridge;
     }
 
     @Override
     public RegisterResponse register(RegisterRequest request) throws PasswordsDoNotMatchException {
-        return null;
+        User user = new User();
+        user.setUsername(request.getUsername());
+
+        if (!request.getPassword().equals(request.getConfirmPassword())) {
+            throw new PasswordsDoNotMatchException();
+        }
+
+        user.setPassword(this.passwordEncoder.encode(request.getPassword()));
+        this.userRepository.saveAndFlush(user);
+
+        RegisterUserModel userModel = this.modelMapper.map(request, RegisterUserModel.class);
+        User userId = this.userRepository.findFirstByUsername(request.getUsername());
+        userModel.setId(userId.getId());
+        this.streamBridge.send(KafkaConstants.USER_PUBLISHER, userModel);
+
+        return this.modelMapper.map(user, RegisterResponse.class);
     }
 
     @Override
@@ -49,5 +74,10 @@ public class UserServiceImpl implements UserService {
         }
 
         return this.jwtUtils.generateJwtToken(user);
+    }
+
+    @Bean
+    public Supplier<Mono<RegisterUserModel>> publishUser() {
+        return () -> Mono.just(new RegisterUserModel());
     }
 }
